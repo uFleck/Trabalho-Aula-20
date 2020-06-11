09856da52abf3addcfd72301f1fbee9b84149c83
Bruno Koster <bruno10762@gmail.com>
2020-06-11 03:05:00 -0300

    fix errors

--- Aula 20/ProjExercicio/src/View/Main.java
+++ Aula 20/ProjExercicio/src/View/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("Seja bem-vindo(a)!");
         
         boolean logado = false;
+        boolean cadastrado = false;
         byte tInicial;
         do{
             System.out.println("1 - Fazer Login             2 - Cadastrar \n3 - Aula                    0 - Sair");
@@ -18,104 +19,122 @@ public class Main {
             
             switch(tInicial){
                 case 1:
-                    boolean voltar = false;
-                    do{
-                        System.out.println("Login:");
-                        System.out.println("Digite seu Login:");
-                        String login = in.next();
-                        if(!login.equals(a1.getLogin()) && voltar == false){
+                    if(cadastrado){
+                        if(logado){
+                            System.out.println("Você já está logado!");
+                        }else{
+                            boolean voltar = false;
                             do{
-                                System.out.println("Login incorreto!");
-                                System.out.println("1 - Tentar novamente        2 - Voltar");
-                                byte opL = in.nextByte();
-                                switch (opL){
-                                    case 1:
+                                System.out.println("Login:");
+                                System.out.println("Digite seu Login:");
+                                String login = in.next();
+                                if(!login.equals(a1.getLogin()) && voltar == false){
+                                    do{
+                                        System.out.println("Login incorreto!");
+                                        System.out.println("1 - Tentar novamente        2 - Voltar");
+                                        byte opL = in.nextByte();
+                                        switch (opL){
+                                            case 1:
+                                                do{
+                                                    System.out.println("Digite seu Login novamente:");
+                                                    login = in.next();
+                                                    if(!login.equals(a1.getLogin())){
+                                                        System.out.println("Login incorreto!");
+                                                        System.out.println("1 - Tentar novamente        2 - Voltar");
+                                                        byte lVoltar =in.nextByte();
+                                                        if(lVoltar == 2){
+                                                            System.out.println("Voltando...");
+                                                            voltar = true;
+                                                            break;
+                                                        }
+                                                    } else{
+                                                        System.out.println("Senha confirmada!");
+                                                        voltar = true;
+                                                    }
+                                                } while(!login.equals(a1.getLogin()) || voltar == false);
+                                                break;
+                                            case 2:
+                                                System.out.println("Voltando...");
+                                                voltar = true;
+                                                break;
+                                        }
+                                    } while(!login.equals(a1.getLogin()) && voltar == false);
+                                }
+
+                                if(login.equals(a1.getLogin())){
+                                    System.out.println("Digite sua Senha:");
+                                    String senha = in.next();
+
+                                    if(!senha.equals(a1.getSenha())){
                                         do{
-                                            System.out.println("Digite seu Login novamente:");
-                                            login = in.next();
-                                            if(!login.equals(a1.getLogin())){
-                                                System.out.println("Login incorreto!");
-                                                System.out.println("1 - Tentar novamente        2 - Voltar");
-                                                byte lVoltar =in.nextByte();
-                                                if(lVoltar == 2){
+                                            System.out.println("Senha incorreta!");
+                                            System.out.println("1 - Tentar novamente        2 - Voltar");
+                                            byte opS = in.nextByte();
+
+                                            switch(opS){
+                                                case 1:
+                                                    do{
+                                                        System.out.println("Digite sua Senha novamente:");
+                                                        senha = in.next();
+                                                        if(!senha.equals(a1.getSenha())){
+                                                            System.out.println("Senha incorreta!");
+                                                            System.out.println("1 - Tentar novamente        2 - Voltar");
+                                                            byte sVoltar = in.nextByte();
+
+                                                            if(sVoltar == 2){
+                                                                System.out.println("Voltando...");
+                                                                voltar = true;
+                                                                break;
+                                                            }
+                                                        } else{
+                                                            System.out.println("Senha confirmada!");
+                                                        }
+                                                    } while(!senha.equals(a1.getSenha()) && voltar == false);
+                                                    break;
+
+                                                case 2:
                                                     System.out.println("Voltando...");
                                                     voltar = true;
-                                                    break;
-                                                }
-                                            } else{
-                                                System.out.println("Senha confirmada!");
-                                                voltar = true;
                                             }
-                                        } while(!login.equals(a1.getLogin()) || voltar == false);
-                                        break;
-                                    case 2:
-                                        System.out.println("Voltando...");
+                                        } while(!senha.equals(a1.getSenha()) && voltar == false);
+                                    } else{
+                                        System.out.println("Você foi logado com sucesso!");
                                         voltar = true;
                                         break;
-                                }
-                            } while(!login.equals(a1.getLogin()) && voltar == false);
-                        }
-                        
-                        if(login.equals(a1.getLogin())){
-                            System.out.println("Digite sua Senha:");
-                            String senha = in.next();
-
-                            if(!senha.equals(a1.getSenha())){
-                                do{
-                                    System.out.println("Senha incorreta!");
-                                    System.out.println("1 - Tentar novamente        2 - Voltar");
-                                    byte opS = in.nextByte();
-
-                                    switch(opS){
-                                        case 1:
-                                            do{
-                                                System.out.println("Digite sua Senha novamente:");
-                                                senha = in.next();
-                                                if(!senha.equals(a1.getSenha())){
-                                                    System.out.println("Senha incorreta!");
-                                                    System.out.println("1 - Tentar novamente        2 - Voltar");
-                                                    byte sVoltar = in.nextByte();
-                                                    
-                                                    if(sVoltar == 2){
-                                                        System.out.println("Voltando...");
-                                                        voltar = true;
-                                                        break;
-                                                    }
-                                                } else{
-                                                    System.out.println("Senha confirmada!");
-                                                }
-                                            } while(!senha.equals(a1.getSenha()) && voltar == false);
-                                            break;
-
-                                        case 2:
-                                            System.out.println("Voltando...");
-                                            voltar = true;
                                     }
-                                } while(!senha.equals(a1.getSenha()) && voltar == false);
-                            }
+                                }
+                            }while(voltar == false);
+                            logado = true;
                         }
-                    }while(voltar == false);
-                    logado = true;
+                    } else{
+                        System.out.println("Cadastre-se primeiro!");
+                    }
                     break;
 
                     
                 case 2:
-                    System.out.println("Cadastro:");
-                    
-                    System.out.println("Digite seu nome:");
-                    in.nextLine();
-                    a1.setNome(in.nextLine());
-                    
-                    System.out.println("Digite seu RA:");
-                    a1.setRa(in.next());
-                    
-                    System.out.println("Digite seu Login:");
-                    a1.setLogin(in.next());
-                    
-                    System.out.println("Digite a sua senha:");
-                    a1.setSenha(in.next());
-                    
-                    System.out.println("Você foi cadastrado com sucesso!");
+                    if(cadastrado){
+                        System.out.println("Você já está cadastrado!");
+                    } else{
+                        System.out.println("Cadastro:");
+
+                        System.out.println("Digite seu nome:");
+                        in.nextLine();
+                        a1.setNome(in.nextLine());
+
+                        System.out.println("Digite seu RA:");
+                        a1.setRa(in.next());
+
+                        System.out.println("Digite seu Login:");
+                        a1.setLogin(in.next());
+
+                        System.out.println("Digite a sua senha:");
+                        a1.setSenha(in.next());
+
+                        System.out.println("Você foi cadastrado com sucesso!");
+
+                        cadastrado = true;
+                    }
                     break;
                     
                 case 3:
